888003ec610dceef447c7031773c6cc29d1fdd22
RAFI <infinityprogrammer01@gmail.com>
2025-05-01 05:49:43 +0000

    first commit

--- ims_support/hooks.py
+++ ims_support/hooks.py
@@ -242,3 +242,17 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+    {"dt": "Property Setter", 
+     "filters": 
+        [
+            [
+                "name",
+                "in",
+                {
+                    "Issue-main-track_views",
+                }
+            ]
+        ]
+    },
+]
